8ad0290248dd3f1d2da458a7544be95efc3d6b47
haibui <buithanhhai2207@gmail.com>
2023-02-23 00:16:11 +0700

    feat: intial project

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,14 @@
+/// <reference types="@emotion/react/types/css-prop" />
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <div>App</div>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
